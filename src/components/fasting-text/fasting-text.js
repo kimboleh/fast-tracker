00c106a28492cd21e3e31e6c4bc4511e6c92f6ed
Kimberly Horton <khorton@vernier.com>
2023-02-21 15:34:31 -0800

    add svg icons

--- src/components/fasting-text/fasting-text.js
+++ src/components/fasting-text/fasting-text.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import meat from '../fast-icons/meat.svg';
+import dairy from '../fast-icons/dairy.svg';
+import fish from '../fast-icons/fish.svg';
+import alcohol from '../fast-icons/alcohol.svg';
+import oil from '../fast-icons/oil.svg';
 
 export class FastingText extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
-      <div>
-      <h1>What can Austin & Kimberly eat?</h1>
-      <p>Austin and Kimberly are participating in the {this.props.fast} right now, {this.props.fastDetails}.
-      </p>
+      <div id="main-content">
+        <div id='fasting' className='main-body'>
+          <h1>What can Austin & Kimberly eat?</h1>
+          <p>Austin and Kimberly are participating in the {this.props.fast} right now, {this.props.fastDetails}.
+          </p>
+          <div id="fast-icons">
+            <img src={meat} class="fast-icon"></img>
+            <img src={dairy} class="fast-icon"></img>
+            <img src={fish} class="fast-icon"></img>
+            <img src={alcohol} class="fast-icon"></img>
+            <img src={oil} class="fast-icon"></img>
+          </div>
+        </div>
       </div>
     );
   }
