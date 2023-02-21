25592dbb0554b176a9ef3bef8e9347ec6ecef7a6
Kimberly Horton <khorton@vernier.com>
2023-02-21 15:34:20 -0800

    re-work data population in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 import {FastingText} from './components/fasting-text/fasting-text.js';
+import {Sidebar} from './components/sidebar/sidebar.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dateString: '',
       isDataLoaded: false,
       fastJSON: {},
       fast: '',
-      fastDetails: ''
+      fastDetails: '',
+      title: ''
     };
+    this.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+    "October", "November", "December"];
+    this.today = new Date();
   }
 
   async componentDidMount() {
@@ -22,9 +28,49 @@ class App extends React.Component {
         isDataLoaded: true});
     })
 
+    console.log(this.state.fastJSON);
+
     if(this.state.isDataLoaded){
+      this.setColor(this.state.fastJSON);
+      this.setDate(this.today, this.state.fastJSON);
       this.setFast();
       this.setFastDetails();
+    }
+  }
+
+  setDate(dateObject, json) {
+    var year = dateObject.getFullYear();
+    var month = dateObject.getMonth();
+    var numericalDay = dateObject.getDate();
+
+    this.setState({dateString: this.months[month] + " " + numericalDay + ", " + year});
+    if (json.titles.length > 0) {
+      this.setState({title: json.titles[0]});
+    }
+  }
+
+  setColor(json) {
+    const body = document.getElementsByTagName('body')[0];
+
+    /* gold - default */
+    body.className = 'gold';
+
+    /* blue - Marian feasts, Presentation, Annunciation, & Akathist */
+
+    /* red - feasts commemorating the cross, Apostles' Fast & Feast, Advent fast,
+     * feast days for holy martyrs, and Great & Holy Thursday */
+
+    /* green - Pentecost, Palm Sunday, & days commemmorating angels, prophets,
+     * monastic saints, ascetics, & fools for Christ */
+
+    /* black/purple - Lent */
+    if (json.pascha_distance >= -40 && json.pascha_distance < 0) {
+      body.className = 'purple';
+    }
+
+    /* white - Epiphany, Transfiguration, & Pascha */
+    if (json.pascha_distance === 0) {
+      body.className = 'white';
     }
   }
 
@@ -36,7 +82,7 @@ class App extends React.Component {
     } else if (apiDay.titles[0].includes("Cheesefare")) {
       this.setState({
         fast: "week of Cheesefare",
-        fastDetails: "no red & white meat. Since it's only a <strong>Meat Fast</strong>, <em>fish, dairy, and alcohol are fine"});
+        fastDetails: "no red & white meat. Since it's only a Meat Fast, fish, dairy, and alcohol are fine"});
     } else {
       this.setState({fast: apiDay.fast_level_desc});
     }
@@ -54,10 +100,10 @@ class App extends React.Component {
     // exceptions
     switch (apiDay.fast_exception) {
       case 2:
-        this.setState({fastDetails: "but today is a <strong>Fish Day</strong>, so only <em>red & white meat and dairy are off-limits</em>"});
+        this.setState({fastDetails: "but today is a Fish Day, so only red & white meat and dairy are off-limits"});
         break;
       case 7:
-        this.setState({fastDetails: "but today is only a <strong>Meat Fast</strong>, so <em>only red & white meats are off-limits</em>"});
+        this.setState({fastDetails: "but today is only a Meat Fast, so only red & white meats are off-limits"});
         break;
       default: break;
 
@@ -66,7 +112,8 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div id="app" className="App">
+        <Sidebar title={this.state.title} dateText={this.state.dateString}></Sidebar>
         <FastingText fast={this.state.fast} fastDetails={this.state.fastDetails} />
       </div>
     );
