559ccca76be3adf021ecf8f953f2efe5a45444a2
Kimberly Horton <khorton@vernier.com>
2023-02-27 14:46:31 -0800

    add next and previous buttons

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './sidebar.css';
+import arrow from '../fast-icons/arrow.svg';
 
 export class Sidebar extends React.Component {
-
-    // <button onClick={this.props.previousDay}>previous</button>
-    // <button onClick={this.props.nextDay}>next</button>
 
   render() {
     return (
@@ -12,13 +10,18 @@ export class Sidebar extends React.Component {
             <div id="title">{this.props.title}</div>
             <div id="dateText">{this.props.dateText}</div>
             <div id="next-previous-buttons">
+                <button onClick={this.props.previousDay}>
+                    <img src={arrow} id="previous-arrow" alt="previous day"></img>
+                </button>
+                <button onClick={this.props.nextDay}>
+                    <img src={arrow} id="next-arrow" alt="next day"></img>
+                </button>
             </div>
             <div id="list-title">Coming soon:</div>
             <div id="list">
                 <ul>
                     <li>Monthly calendar view</li>
-                    <li>Date picker</li>
-                    <li>Info page with fasting basics</li>
+                    <li>About page & FAQ</li>
                 </ul>
             </div>
         </div>
